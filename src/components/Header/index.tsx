281502563425e2da7faadac9e0c157ac88118133
Ariel Betti <ariel.betti@gmail.com>
2023-01-31 02:10:01 -0300

    feat: create header component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,26 +1,40 @@
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
+
+// assets
+import pattern from "../../assets/pattern.svg";
 
 // icons
-import { EyeIcon } from "@heroicons/react/24/outline"
+import { PlusSmallIcon } from "@heroicons/react/24/outline";
 
 // paths
-import { PATHS } from "../../core/paths"
+import { PATHS } from "../../core/paths";
+
+// components
+import { Button, ThemeToggle } from "../";
 
 // ::
 const Header = () => {
   return (
-    <div className='bg-zinc-800 fixed top-0 left-0 w-full'>
-      <div className="container h-16 mx-auto px-4 flex items-center justify-between">
-        <Link to={PATHS.create} className="flex gap-1 text-xl items-center justify-start">
-          <EyeIcon className="h-6 w-6" />
-          Confidential
+    <div className="fixed top-0 left-0 z-30 w-full bg-zinc-800/5 backdrop-blur-md">
+      <div className="container mx-auto flex h-16 items-center justify-between px-4">
+        <Link to={PATHS.create}>
+          <img
+            className="h-12 w-12 rounded-full object-cover hover:border-2 hover:border-blue-300 hover:shadow-[0px_0px_5px] hover:shadow-blue-400 hover:transition-all"
+            src={pattern}
+            alt="Logo"
+          />
         </Link>
-        <div>
-          Links
+        <div className="flex gap-2">
+          <Button asChild>
+            <Link to={PATHS.create}>
+              <PlusSmallIcon className="h-5 w-5" /> Message
+            </Link>
+          </Button>
+          <ThemeToggle />
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
