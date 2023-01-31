175d015a62159ba57ff1551ea29829231cad0061
Ariel Betti <ariel.betti@gmail.com>
2023-01-31 02:12:10 -0300

    chore: code cleaning

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,12 @@
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+// components
 import { Header } from './components';
+
+// routes
 import AppRoutes from './core/routes';
 
 import './global.css';
