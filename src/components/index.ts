8accac4818f06dfa0426e96ec7530f2b878a6be3
Ariel Betti <ariel.betti@gmail.com>
2023-01-31 02:11:35 -0300

    feat: export all components

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,11 +1,21 @@
+import BackdropLoading from "./BackdropLoading";
 import Button from "./Button";
+import Card from "./Card";
 import Header from "./Header";
 import Input from "./Input";
+import LoadingStatus from "./LoadingStatus";
+import Tapume from "./Tapume";
 import TextEditor from "./TextEditor";
+import ThemeToggle from "./ThemeToggle";
 
 export {
+  BackdropLoading,
   Button,
+  Card,
   Header,
   Input,
+  LoadingStatus,
+  Tapume,
   TextEditor,
+  ThemeToggle,
 }
